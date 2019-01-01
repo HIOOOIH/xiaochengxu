5a6c2aa0881908f9f63b62f612a6f5c28ef1cce9
HIOOOIH <735063697@qq.com>
2019-01-01 20:33:31 +0800

    add search

--- components/search/index.js
+++ components/search/index.js
@@ -1,23 +1,82 @@
 // components/search/index.js
+
+import { KeywordModel } from "../../models/keyword.js";
+
+const KeyWordModel = new KeywordModel();
+
 Component({
+  // 启用多 slot 支持
+  options: {
+    multipleSlots: true
+  },
+
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
+    historyWords: [],
+    hotWords: [],
+    dataArray: [],
+    searching: false,
+    q: ""
+  },
 
+  attached: function() {
+    const historyWords = KeyWordModel.getHistory();
+    const hotWords = KeyWordModel.getHot();
+    this.setData({
+      historyWords: historyWords
+    });
+
+    hotWords.then(res => {
+      this.setData({
+        hotWords: res.hot
+      });
+    });
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    // 关闭搜索
+    onCancel() {
+      this.triggerEvent("cancel", {}, {});
+    },
+    // 点击或回车后行为
+    onConfirm() {
+      this.setData({
+        searching: true
+      });
+      // 加入缓存
+      // const word = event.detail.value;
+      // KeyWordModel.addToHistory(word);
 
+      // 监听后去服务器请求数据
+      const q = event.detail.value || event.detail.text;
+      KeyWordModel.search(0, q).then(res => {
+        this.setData({
+          dataArray: res.books,
+          q: q
+        });
+
+        // 修改后，当服务器返回数据后，认为用户输入有效，有返回结果，再写入缓存中
+        KeyWordModel.addToHistory(q);
+      });
+    },
+
+    // 清空搜索
+    onDelete() {
+      this.setData({
+        searching: false,
+        dataArray: [],
+        q: ""
+      });
+    }
   }
-})
+});
---
+++ models/keyword.js
@@ -0,0 +1,53 @@
+import { HTTP } from "../util/http-p.js";
+
+class KeywordModel extends HTTP {
+  key = "q";
+  maxLength = 10;
+  // 获取搜索历史
+  getHistory() {
+    let words = wx.getStorageSync(this.key);
+    if (!words) {
+      return [];
+    }
+    return words;
+  }
+
+  // 获取最热搜索词
+  getHot() {
+    return this.request({
+      url: "/book/hot_keyword"
+    });
+  }
+
+  // 加入缓存
+  addToHistory(keyword) {
+    let words = this.getHistory();
+    // 判断是否存在
+    const hasWords = words.includes(keyword);
+    // 队列
+    if (!hasWords) {
+      const length = words.length;
+      if (length >= this.maxLength) {
+        // 删除末尾元素
+        words.pop();
+      }
+      // 添加到元素首位
+      words.unshift(keyword);
+      // 存入缓存
+      wx.setStorageSync(this.key, words);
+    }
+  }
+
+  // 搜索
+  search(start, q) {
+    return this.request({
+      url: "/book/search?summary=1",
+      data: {
+        q: q,
+        start: start
+      }
+    });
+  }
+}
+
+export { KeywordModel };
